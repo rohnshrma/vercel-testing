1be34fb8a849b717d327523fedf3f9546755bbee
rohnshrma <rohn11shrma@gmail.com>
2024-03-07 12:48:05 +0530

    vercel-testing

---
+++ app.js
@@ -0,0 +1,17 @@
+const express= require("express")
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+
+app.listen(PORT,()=>{
+    console.log("Server started on port ", PORT);
+})
+
+
+
+
+
